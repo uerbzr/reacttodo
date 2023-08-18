615fe8c2105cc42ce21ffa9de3ad405af9eb85a8
uerbzr <nigel.sibbert@gmail.com>
2023-08-18 12:08:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+  const [tasks, setTasks] = useState([
+    { text: 'Go to gym', checked: true },
+    { text: 'Drink coffee', checked: false },
+])
+
+const handleSubmit = (event) => {
+  event.preventDefault()
+  console.log(event.target[0].value)
+  setTasks([...tasks, {text: event.target[0].value, checked:false }])
+}
+
+const handleDelete = (index) => {
+    console.log(tasks, tasks.filter(item => {
+      
+    }))
+
+    const updated = tasks.filter((todoItem, todoIndex) => todoIndex !== index)
+    setTasks([...updated])
+}
+
+const handleUpdate = (index, checked) => {
+    // TODO Find the task by the provided index
+    // Change the checked property on the task
+    // Update the state array to re-render the component
+    // HINT: .map() or access by index
+    console.log(tasks[index].checked = checked)
+
+    const updated = tasks[index].checked = checked;
+    setTasks([...updated])
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='app'>
+    <main>
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="task" />
+            <button type="submit">add task</button>
+        </form>
+
+        {
+            tasks.map((todoItem, index) => {
+                return (
+                    <div>
+                        <span>{todoItem.text}</span>
+                        <input onChange={(event) => handleUpdate(index, event.target.checked)} type="checkbox" checked={todoItem.checked} />
+                        <small className="deleteBtn" onClick={() => handleDelete(index)}>Delete</small>
+                    </div>
+                )
+            })
+        }
+    </main>
+</div>
   );
 }
 
